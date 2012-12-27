634d79214bf3faab3f9f94f3f7594960f9b46e8d
François Parmentier <francois.parmentier@gmail.com>
2012-12-27 22:06:33 +0100

    Fix a bug about last word of a sentence.

--- test/test.js
+++ test/test.js
@@ -28,29 +28,39 @@ describe('Tokenizer creations', function () {
 
 describe('Sentences token', function () {
   var tokenizer = new Tokenizer('François');
-  var entry = "Nous allons bien voir ce que ça   donne!" +
-  " N'est-ce pas ? " + 
-  " Et avec une URL en plus, c'est mieux: http://google.com." +
-  " Mais il nous manque encore un mail: gg@gggg.kk";
-  tokenizer.setEntry(entry);
-  var sentences = tokenizer.getSentences();
-  it("should get 4 sentences", function () {
-    assert.equal(4, sentences.length);
+  describe('Classic', function () {
+    var entry = "Nous allons bien voir ce que ça   donne!" +
+    " N'est-ce pas ? " + 
+    " Et avec une URL en plus, c'est mieux: http://google.com." +
+    " Mais il nous manque encore un mail: gg@gggg.kk";
+    tokenizer.setEntry(entry);
+    var sentences = tokenizer.getSentences();
+    it("should get 4 sentences", function () {
+      assert.equal(4, sentences.length);
+    });
+    it("should have the first sentence", function () {
+      assert.equal("Nous allons bien voir ce que ça donne!",
+        sentences[0]);
+    });
+    it("should have second sentence", function () {
+      assert.equal("N'est-ce pas ?",
+        sentences[1]);
+    });
+    it("should have third sentence", function () {
+      assert.equal("Et avec une URL en plus, c'est mieux: http://google.com.",
+        sentences[2]);
+    });
+    it("should have fourth sentence", function () {
+      assert.equal("Mais il nous manque encore un mail: gg@gggg.kk",
+        sentences[3]);
+    });    
   });
-  it("should have the first sentence", function () {
-    assert.equal("Nous allons bien voir ce que ça donne!",
-      sentences[0]);
-  });
-  it("should have second sentence", function () {
-    assert.equal("N'est-ce pas ?",
-      sentences[1]);
-  });
-  it("should have third sentence", function () {
-    assert.equal("Et avec une URL en plus, c'est mieux: http://google.com.",
-      sentences[2]);
-  });
-  it("should have fourth sentence", function () {
-    assert.equal("Mais il nous manque encore un mail: gg@gggg.kk",
-      sentences[3]);
+  describe('False end', function () {
+    var entry = "Bon sang ce n'est pas ça. Bon sang";
+    tokenizer.setEntry(entry);
+    var sentences = tokenizer.getSentences();
+    it('should produce only 2 sentences', function () {
+      assert.equal(2, sentences.length);
+    });
   });
 });
